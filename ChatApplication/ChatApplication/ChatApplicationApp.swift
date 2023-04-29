78cb43e29d7da1aa2fcbb3be0c0f5fe93eb1ec56
saikumar784243 <saikumar784243@gmail.com>
2023-04-29 18:18:50 +0530

    Project created

---
+++ ChatApplication/ChatApplication/ChatApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApplicationApp.swift
+//  ChatApplication
+//
+//  Created by sai kumar on 29/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
